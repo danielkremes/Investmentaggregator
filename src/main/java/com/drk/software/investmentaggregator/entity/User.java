81131795a4552f2847c5434e0ee454a30160c45e
danielkremes <danielkremes06@gmail.com>
2025-06-29 15:32:14 -0300

    feature: Create model database entity, name class User

---
+++ src/main/java/com/drk/software/investmentaggregator/entity/User.java
@@ -0,0 +1,30 @@
+package com.drk.software.investmentaggregator.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_user")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "username_id")
+    private UUID usernameId;
+
+    @Column(name = "name_user")
+    private String nameUser;
+
+    @Column(name = "email_user")
+    private String emailUser;
+
+
+
+}
